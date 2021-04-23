1b6f37d7bd72f75bd5f828968fe6c3d45c27fbee
Nat Kershaw <nakersha@microsoft.com>
2021-04-22 17:02:08 -0700

    RE-run with file annotation

--- html/menudata.js
+++ html/menudata.js
@@ -51,4 +51,8 @@ var menudata={children:[
 {text:"s",url:"functions_vars.html#index_s"},
 {text:"t",url:"functions_vars.html#index_t"}]}]}]},
 {text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"File List",url:"files.html"},
+{text:"Globals",url:"globals.html",children:[
+{text:"All",url:"globals.html"},
+{text:"Typedefs",url:"globals_type.html"},
+{text:"Enumerations",url:"globals_enum.html"}]}]}]}
